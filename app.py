8f61f6b1e1feb84a60bd3e4c2ed651d054f1ade7
mahdiiii04 <143207634+mahdiiii04@users.noreply.github.com>
2023-08-31 14:06:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def recommend_anime():
     return render_template('index.html', names=animes_list, recommended=recommended, chosen=[])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0")
