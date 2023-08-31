e0972d85088517524fe46c7fe4d576fad6b7d26f
mahdiiii04 <143207634+mahdiiii04@users.noreply.github.com>
2023-08-31 14:07:25 +0100

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,22 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-var rec_button = document.getElementById("rec")
+const rec_button = document.getElementById("rec")
+const add_button = document.getElementById("add")
+const clear_button = document.getElementById("clear")
+const buttonsParent = document.getElementById("buttons")
 
 
-rec_button.addEventListener('click', ()=>{
-    console.log("Button clicked");
+add_button.addEventListener('click', ()=>{
     var select = document.getElementById("Anime_Dropdown");
     var selectedOption = select.options[select.selectedIndex];
-    var anime_name = selectedOption.value;
-
+    var anime_name = selectedOption.textContent;        
+    
     var form = document.createElement('form');
     form.style.display = 'none';
 
     form.method = 'POST';
-    form.action = '/recommend_anime';
+    form.action = '/adding';
 
     var input = document.createElement('input');
     input.type = 'hidden';
@@ -27,4 +29,55 @@ rec_button.addEventListener('click', ()=>{
 
     form.submit();
 });
+
+rec_button.addEventListener('click', ()=>{       
+
+    const rec_send = document.createElement('form');
+    rec_send.method = 'POST';
+    rec_send.action = '/recommend_anime';
+
+    document.body.appendChild(rec_send);
+
+    rec_send.submit();
 });
+
+clear_button.addEventListener('click', ()=>{
+    const clear_send = document.createElement('form');
+    clear_send.method = 'POST';
+    clear_send.action = '/';
+
+    document.body.appendChild(clear_send);
+
+    clear_send.submit();
+});
+
+
+
+function ButtonClickedHandler (event){
+    var btn = event.target;
+    var id = btn.id;
+
+    var form = document.createElement('form');
+    form.style.display = 'none';
+
+    form.method = 'POST';
+    form.action = '/deleting';
+
+    var input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = 'ID';
+    input.value = id;
+
+    form.appendChild(input);
+
+    document.body.appendChild(form);
+
+    form.submit();
+
+};
+
+buttonsParent.addEventListener('click', ButtonClickedHandler);
+
+
+});
+
